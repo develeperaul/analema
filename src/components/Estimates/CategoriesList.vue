49122f979bb60c1cfd6741bcd732a89336bbe4d9
Nikita Antonov <nikita45454@gmail.com>
2024-12-26 21:01:38 +0300

    feat: estimates - добавляет CategoriesList

---
+++ src/components/Estimates/CategoriesList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="h2 tw-mb-3">Выберите категорию</div>
+    <div class="area" :class="{ 'area--overflow': isOverflow && !showed }" ref="areaRef">
+      <div class="items">
+        <BaseRadio
+          class="tw-mb-3"
+          v-for="section in sections"
+          :key="section.id"
+          name="section"
+          :label="section.name"
+          :checkedValue="section.id"
+          v-model="value"
+        />
+      </div>
+      <div v-if="!showed && isOverflow" class="shadow-line"></div>
+    </div>
+    <button
+      v-if="isOverflow"
+      type="button"
+      class="action"
+      @click="showed = !showed"
+    >
+      {{ showed ? 'Скрыть' : 'Показать еще' }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import type { Section } from 'src/repositories/estimates';
+  import BaseRadio from 'src/components/Base/Radio.vue';
+
+  defineProps<{
+    sections: Section[],
+  }>();
+
+  const value = defineModel<string>({
+    default: '',
+  });
+
+  const areaRef = ref<HTMLElement | null>(null);
+
+  const showed = ref(false);
+
+  const isOverflow = ref(false);
+
+  function checkOverflow() {
+    if(areaRef.value) {
+      isOverflow.value = areaRef.value.offsetHeight > 250;
+    }
+  }
+
+  onMounted(() => {
+    checkOverflow();
+  });
+</script>
+
+<style scoped lang="scss">
+  .area {
+    position: relative;
+
+    &--overflow {
+      height: 250px;
+      overflow: hidden;
+    }
+  }
+
+  .action {
+    @apply tw-text-blue tw-text-t2 tw-mt-1;
+  }
+
+  .shadow-line {
+    @apply tw-shadow-blur tw-bg-white tw-h-4 tw-w-full tw-absolute tw-bottom-0 tw-left-0;
+  }
+</style>
