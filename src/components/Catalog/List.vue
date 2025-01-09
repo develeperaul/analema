501d563ba54cc310aa85b89edfb62d48f67257d1
Nikita Antonov <nikita45454@gmail.com>
2025-01-09 13:06:23 +0300

    feat: catalog - добавляет список товаров

--- src/components/Catalog/List.vue
+++ src/components/Catalog/List.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <ListItem
-      v-for="item in items"
-    />
+    <p v-if="items.length === 0">Не найдено ни одного товара</p>
+    <div v-else class="list">
+      <ListItem
+        class="item"
+        v-for="item in items"
+        :item="item"
+      />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import ListItem from './ListItem.vue';
+  import type { CatalogItem } from 'src/repositories/catalog';
 
   defineProps<{
-    items: [],
+    items: CatalogItem[],
   }>();
 </script>
+
+<style scoped lang="scss">
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -8px -6px;
+  }
+
+  .item {
+    margin: 8px 6px;
+    width: calc(50% - 12px);
+  }
+</style>
--- src/components/Catalog/ListItem.vue
+++ src/components/Catalog/ListItem.vue
@@ -1,9 +1,38 @@
 <template>
-  <router-link to="/">
-    1
+  <router-link class="catalog-item" to="/">
+    <Image imgClass="tw-w-full" width="162" height="142" :src="item.img" />
+    <div class="body">
+      <div class="price">{{ $amount(item.price) }}</div>
+      <div class="name">{{ item.name }}</div>
+    </div>
   </router-link>
 </template>
 
 <script setup lang="ts">
+  import type { CatalogItem } from 'src/repositories/catalog';
 
+  const props = defineProps<{
+    item: CatalogItem,
+  }>();
 </script>
+
+
+<style scoped lang="scss">
+  .catalog-item {
+    border-radius: 10px;
+    overflow: hidden;
+    @apply tw-bg-card;
+  }
+
+  .body {
+    padding: 12px 5px 13px 10px;
+  }
+
+  .price {
+    @apply tw-text-t1 tw-font-bold tw-mb-1;
+  }
+
+  .name {
+    @apply tw-text-t2 tw-text-card-descr;
+  }
+</style>
