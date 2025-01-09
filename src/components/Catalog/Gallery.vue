880af34a1694034e59b78c8e1b78b920c3cd27cf
Nikita Antonov <nikita45454@gmail.com>
2025-01-09 20:51:28 +0300

    feat: catalog - добавляет Gallery

---
+++ src/components/Catalog/Gallery.vue
@@ -0,0 +1,20 @@
+<template>
+  <Swiper
+    :slidesPerView="1.1"
+    :spaceBetween="8"
+  >
+    <SwiperSlide v-for="image in images">
+      <Image class="tw-rounded-20 tw-overflow-hidden" imgClass="tw-w-full" :width="width" :height="height" :src="image" />
+    </SwiperSlide>
+  </Swiper>
+</template>
+
+<script setup lang="ts">
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  defineProps<{
+    width?: number | string,
+    height?: number | string,
+    images: string[],
+  }>();
+</script>
