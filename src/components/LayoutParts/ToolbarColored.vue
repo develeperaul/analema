b4ed2789a8014e290d2bd3689c4550c187c9c772
Nikita Antonov <nikita45454@gmail.com>
2025-01-21 13:44:35 +0300

    feat: добавляет ToolbarColored

---
+++ src/components/LayoutParts/ToolbarColored.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="toolbar-colored">
+    <div class="tw-mb-4" :class="btnWrapClass">
+      <button class="btn-back" type="button" @click="router.back">
+        <BaseIcon name="back" fit />
+      </button>
+    </div>
+    <div class="body">
+      <div class="icon-wrap" :class="iconWrapClass">
+        <BaseIcon :name="icon" fit />
+      </div>
+      <h1 v-if="title" class="tw-text-h1 tw-text-el tw-font-bold tw-mt-3">
+        {{ title }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+
+  defineProps<{
+    icon: string,
+    iconWrapClass?: string,
+    title?: string,
+    btnWrapClass?: string,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .btn-back {
+    width: 32px;
+    height: 32px;
+    padding: 6px;
+  }
+
+  .icon-wrap {
+    @apply tw-rounded-20 tw-bg-el;
+  }
+
+  .toolbar-colored {
+    border-radius: 0 0 24px 24px;
+    padding: 16px;
+    padding-bottom: 35px;
+  }
+
+  .body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
