6083117a8c2d7f69d52668c84afe10cc065ab5c8
Nikita Antonov <nikita45454@gmail.com>
2025-02-27 10:56:03 +0300

    fix: formdata - исправляет прикрепление файлов

--- src/utils/formdata.ts
+++ src/utils/formdata.ts
@@ -1,7 +1,7 @@
 export function jsonFormData<T extends Record<string, any>>(json: T, parentForm?: FormData, parentKey?: string) {
   const formData = parentForm ?? (new FormData());
   for(let key in json) {
-    if(typeof json[key] === 'object') {
+    if(typeof json[key] === 'object' && !((json[key] as object) instanceof File)) {
       jsonFormData(
         json[key],
         formData,
