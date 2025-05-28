ac83d48df52fd089c841e9b668ba780d744a6e8e
Nikita Antonov <nikita45454@gmail.com>
2025-05-28 10:22:13 +0500

    feat: добавляет модуль config

---
+++ src/boot/config.ts
@@ -0,0 +1,21 @@
+import { boot } from 'quasar/wrappers';
+import { InjectionKey } from 'vue';
+import { inject } from 'vue';
+
+function makeConfig() {
+  return {
+    userAgrement: 'https://analemmatrade.ru/useragr.pdf',
+  };
+}
+
+const configKey: InjectionKey<ReturnType<typeof makeConfig>> = Symbol('config');
+
+export default boot(({ app }) => {
+  app.provide(configKey, makeConfig());
+});
+
+export function useConfig() {
+  const config = inject(configKey);
+  if(!config) throw new Error('config не найден!');
+  return config;
+}
