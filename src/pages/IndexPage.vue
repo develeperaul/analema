3049f968465178f43596b50600fc0aca262fe5c8
Nikita Antonov <nikita45454@gmail.com>
2025-05-28 10:21:42 +0500

    style: главная - заменяет стили блока "Экспресс оценка"

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="wrapper tw-pt-7 page-pb tw-flex tw-flex-col tw-gap-5">
-    <router-link :to="{ name: 'estimates.create' }" class="online-scene">
-      <div class="online-scene__title">Продайте свой товар</div>
-      <div class="online-scene__name">Получите деньги за 10 минут</div>
-      <img src="~assets/images/wallet.png" alt="" class="online-scene__icon" />
-    </router-link>
+    <EstimateBanner />
     <PopularList />
     <div class="tw-grid tw-grid-cols-2 tw-gap-2">
       <LinkCard
@@ -27,4 +23,5 @@
 <script setup lang="ts">
   import PopularList from 'src/components/Catalog/PopularList.vue';
   import LinkCard from 'src/components/Home/LinkCard.vue';
+  import EstimateBanner from 'src/components/Home/EstimateBanner.vue';
 </script>
