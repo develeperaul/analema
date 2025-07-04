1d6f104d8b861f45ff9a427050f7a3f59c175486
Nikita Antonov <nikita45454@gmail.com>
2025-07-04 18:40:18 +0500

    feat: profile - добавляет валидацию для имени и фамилии

--- src/schemas/profile.ts
+++ src/schemas/profile.ts
@@ -1,9 +1,11 @@
 import { string } from 'yup';
 import { emailTest } from 'src/utils/validation';
 
+const nameTest = /^([a-zA-zа-яА-Я]+)(\-[a-zA-zа-яА-Я]+)*$/gi;
+
 export const profileSchema = {
-  name: string().required().label('Имя'),
-  last_name: string().required().label('Фамилия'),
+  name: string().required().matches(nameTest, { message: 'Введите корректное имя' }).label('Имя'),
+  last_name: string().required().matches(nameTest, { message: 'Введите корректную фамилию' }).label('Фамилия'),
   otch: string().label('Отчество'),
   db: string().label('Дата рождения'),
   email: string().test(...emailTest).label('Email'),
