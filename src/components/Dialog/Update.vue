03837ad2eac266efbd0e3060c8922b9c35c54ce9
Nikita Antonov <nikita45454@gmail.com>
2025-09-12 11:14:33 +0300

    style: обновляет стили экрана обновления

--- src/components/Dialog/Update.vue
+++ src/components/Dialog/Update.vue
@@ -5,16 +5,15 @@
         <div class="tw-w-[100px] tw-mx-auto tw-mb-8">
           <img width="100" height="100" src="~/assets/images/logo.svg" />
         </div>
-        <div class="tw-text-h1 tw-font-bold tw-mb-2">Обновите приложение</div>
-        <div class="tw-text-t1 tw-mb-8">
-          Для повышения стабильности, пожалуйста,<br/>обновите приложение до последней вверсии
+        <div class="tw-text-h1 tw-font-bold tw-mb-14">
+          Пожалуйста,<br />обновите приложение<br />до последней версии
         </div>
         <a
           class="tw-block tw-w-full tw-text-center tw-bg-[#181818] tw-rounded-16 tw-text-white tw-py-[13px] tw-px-8 tw-text-t1"
           :href="storeLink"
           target="_blank"
         >
-          Пора обновляться
+          Обновить
         </a>
       </div>
       </div>
@@ -27,14 +26,14 @@
   import { ref } from 'vue';
 
   const $q = useQuasar();
-  const value = ref(false);
+  const value = ref(true);
   const system = useSystem();
   const api = useRepositories();
   const storeLink = $q.platform.is.ios ? system.appStoreLink : system.googlePlayLink;
 
   const res = useRequest(api.system.version, {
     onSuccess() {
-      value.value = hasCriticalUpdate();
+      // value.value = hasCriticalUpdate();
     },
   });
 
